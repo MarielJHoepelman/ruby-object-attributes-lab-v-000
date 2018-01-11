38a8766711e082a9b65b96dc413bb4ecfb2e081a
Mariel Hoepelman <marielhoepelman@gmail.com>
2018-01-11 19:15:09 +0000

    automatically backed up by learn

--- lib/person.rb
+++ lib/person.rb
@@ -7,4 +7,20 @@ class Person
   def name
     @name
   end
+
+  def job=(person_job)
+    @job = person_job
+  end
 end
+
+mariel = Person.new("mariel")
+
+mariel.name
+
+puts  mariel.name
+
+navigator = Person.job
+
+mariel.job = "navigator"
+
+puts mariel.job 
